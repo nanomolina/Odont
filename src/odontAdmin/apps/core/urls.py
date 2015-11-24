e9f98cf67b1f38d761cc09ab7d8f1099533d99a4
nanomolina <nanomolinacav@gmail.com>
2015-11-24 00:28:32 -0300

    ODT-002 se borran estaticos

--- src/odontAdmin/apps/core/urls.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
-
-urlpatterns = [
-    url(r'^', 'apps.core.views.home', name='home'),
-]
--- src/odontAdmin/apps/dental_office/urls.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
-
-urlpatterns = [
-    url(r'^', 'apps.dental_office.views.ejemplo', name='ejemplo'),
-]
